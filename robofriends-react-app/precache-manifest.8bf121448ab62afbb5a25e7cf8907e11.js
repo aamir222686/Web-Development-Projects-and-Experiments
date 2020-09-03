9111f1247ba5af80553ae9cab30672a94bafce95
Aamir Hussain <aamir222686@yahoo.com>
2020-09-03 13:31:36 +0530

    robofriends-react-showcase

---
+++ robofriends-react-app/precache-manifest.8bf121448ab62afbb5a25e7cf8907e11.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9a6201557621d0821d02c8a5a7a90e6",
+    "url": "/Web-Development-Projects-and-Experiments/robofriends-react-app/index.html"
+  },
+  {
+    "revision": "939c29a12718c22b5acf",
+    "url": "/Web-Development-Projects-and-Experiments/robofriends-react-app/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "d2533ea87ec47109a9f3",
+    "url": "/Web-Development-Projects-and-Experiments/robofriends-react-app/static/css/main.aced20aa.chunk.css"
+  },
+  {
+    "revision": "939c29a12718c22b5acf",
+    "url": "/Web-Development-Projects-and-Experiments/robofriends-react-app/static/js/2.e7e24cc0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Web-Development-Projects-and-Experiments/robofriends-react-app/static/js/2.e7e24cc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2533ea87ec47109a9f3",
+    "url": "/Web-Development-Projects-and-Experiments/robofriends-react-app/static/js/main.dddd068d.chunk.js"
+  },
+  {
+    "revision": "aaac820181cf8ee745c4",
+    "url": "/Web-Development-Projects-and-Experiments/robofriends-react-app/static/js/runtime-main.3820c48c.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/Web-Development-Projects-and-Experiments/robofriends-react-app/static/media/SEGA.12a3db8b.woff"
+  }
+]);
