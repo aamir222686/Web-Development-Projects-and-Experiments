c76437e49690cd2f967bdc81f8b67b25b86d11dd
Aamir Hussain <aamir222686@yahoo.com>
2020-09-03 13:04:53 +0530

    new project showcasing React App

---
+++ robofriends-showcase-react/precache-manifest.fe04c67f3881cce7c7dea4d260737fc9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95666591eed2f083694b91fcb3468f40",
+    "url": "/index.html"
+  },
+  {
+    "revision": "939c29a12718c22b5acf",
+    "url": "/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "0adf4bbdb8c7167f22c9",
+    "url": "/static/css/main.83225d69.chunk.css"
+  },
+  {
+    "revision": "939c29a12718c22b5acf",
+    "url": "/static/js/2.e7e24cc0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e7e24cc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0adf4bbdb8c7167f22c9",
+    "url": "/static/js/main.d83022d0.chunk.js"
+  },
+  {
+    "revision": "af1ee15f3b8733767315",
+    "url": "/static/js/runtime-main.8a8ac9cd.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
